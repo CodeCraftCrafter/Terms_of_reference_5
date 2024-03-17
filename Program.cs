35705d21acbc7cd7bb81b614c77231aa8e8ba250
CodeCraftCrafter <163725592+CodeCraftCrafter@users.noreply.github.com>
2024-03-17 14:33:48 +0200

    Terms of reference 4.1
    Materials for lesson 5. Problem 4.1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Xml.Linq;
+
+class Program
+{
+    static void Main()
+    {
+                int[] array = new int[50];
+
+               for (int i = 0; i < 50; i++)
+        {
+            array[i] = 2 + i * 2;
+        }
+        Console.WriteLine("Начало вывода массива:");
+        foreach (int element in array)
+        {
+            Console.WriteLine("Element: " + element);
+        }
+    }
+}
