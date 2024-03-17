09ee9ef886645c5a789b874764118baad6126d07
CodeCraftCrafter <163725592+CodeCraftCrafter@users.noreply.github.com>
2024-03-17 14:40:22 +0200

    Terms of reference 4.3
    Materials for lesson 5. Problem 4.3

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        StringBuilder inputText = new StringBuilder();
+        Console.WriteLine("Введите текст (введите 'end' для завершения ввода):");
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input.ToLower() == "end")
+            {
+                break;
+            }
+            inputText.Append(input + Environment.NewLine);
+        }
+
+        string finalText = inputText.ToString();
+        Console.WriteLine($"Общее количество символов: {finalText.Length}");
+
+        Console.WriteLine("Введите символ для поиска:");
+        char searchCharacter = Console.ReadLine()[0];
+
+        int count = 0;
+
+        foreach (char c in finalText)
+        {
+            if (c == searchCharacter)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine($"Символ '{searchCharacter}' найден {count} раз(а) в введенном тексте.");
+    }
+}
