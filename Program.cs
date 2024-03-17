5e3b3159e172ee6a2b4aa2af95ac4a4446e0b8a8
CodeCraftCrafter <163725592+CodeCraftCrafter@users.noreply.github.com>
2024-03-17 14:36:10 +0200

    Terms of reference 4.2
    Materials for lesson 5. Problem 4.2

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = new int[50];
+        Random random = new Random();
+        int evenCount = 0;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = random.Next(1, 101);
+
+            if (numbers[i] % 2 == 0)
+            {
+                evenCount++;
+            }
+        }
+        Console.WriteLine("Сгенерированные числа:");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+
+        Console.WriteLine("Количество четных чисел: " + evenCount);
+    }
+}
